b656e0865cdcb5fd9d1a544068be18c1cba7ee01
Job85 <julianjernigan@ymail.com>
2023-03-10 13:43:25 -0500

    added console.log to post('/register')

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/AuthController')
 const middleware = require('../middleware')
 
 Router.post('/login', controller.Login)
-Router.post('/register', controller.Register)
+Router.post('/register', controller.Register, () => console.log('In post /register!'))
 Router.put('/update/:user_id',
     middleware.stripToken,
     middleware.verifyToken,
