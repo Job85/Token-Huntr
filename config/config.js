29c3aca1379faacd6d91e92f1977a1fe49852436
Job85 <julianjernigan@ymail.com>
2023-03-09 23:26:13 -0500

    added DATABASE_URL for production environment

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    database: "token_huntr_production",
+    use_env_variable: 'DATABASE_URL',
     dialect: "postgres",
     dialectOptions: {
       ssl: {
