381aff8e97efaf0fd8a290e3296b6bc3265db30d
Job85 <julianjernigan@ymail.com>
2023-03-11 17:39:28 -0500

    required dotenv, added app variable

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const app = express();
+// const { Sequlize } = require('sequelize');
+// const path = require('path');
+const cors = require('cors');
 
-const app = express()
-
-const PORT = process.env.PORT || 3001
-
+require('dotenv').config();
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/api', AppRouter)
+
 const AppRouter = require('./routes/AppRouter')
 
+const PORT = process.env.PORT || 3001
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
-app.use('/api', AppRouter)
+
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
