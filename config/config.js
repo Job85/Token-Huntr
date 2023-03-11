e5392c8c313ad4f5b74dbe98a1c24ee6e5908dae
Job85 <julianjernigan@ymail.com>
2023-03-11 17:03:42 -0500

    converted file .json => .js

---
+++ config/config.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    database: "token_huntr_development",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  test: {
+    database: "token_huntr_test",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: "postgress"
+  }
+}
