e108e5fb70afda458d6209b00ed4ffe759e4ac3c
Job85 <joulescodinglab@gmail.com>
2023-03-12 00:32:28 -0500

    restructured configuration so AppRouter not accessed before initialization

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
-// const { Sequlize } = require('sequelize');
-// const path = require('path');
 const cors = require('cors');
 
-require('dotenv').config();
+const app = express();
+
+const PORT = process.env.PORT || 3001
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/api', AppRouter)
-
 const AppRouter = require('./routes/AppRouter')
 
-const PORT = process.env.PORT || 3001
-app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 
+app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+app.use('/api', AppRouter)
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
